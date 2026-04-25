208bcd8e27bc64de5b853b6b7fec0deae44d6688
netsanetamanuel <abrehotamanuell@gmail.com>
2026-04-25 06:28:36 -0600

    update: an update on home resonpsiveness

---
+++ Frontend/script/script.js
@@ -0,0 +1,8 @@
+function showsidebar(){
+    const sidebar = document.querySelector('.side_bar');
+    sidebar.style.display = 'flex';
+}
+function hidesidebar(){
+     const sidebar = document.querySelector('.side_bar');
+    sidebar.style.display = 'none';
+}
